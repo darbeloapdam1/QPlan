4c4625c01cc1dd548f26a18e261c3f6cf63abaa0
darbeloapdam1 <darbeloapdam1@gmail.com>
2022-05-02 21:13:14 +0200

    crear model base

---
+++ QPlan/QPlan/Models/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QPlan.Models
+{
+    public class BaseModel
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string foto { get; set; }
+        public BaseModel()
+        {
+
+        }
+    }
+}
--- QPlan/QPlan/Models/Evento.cs
+++ QPlan/QPlan/Models/Evento.cs
@@ -4,15 +4,12 @@ using System.Text;
 
 namespace QPlan.Models
 {
-    public class Evento
-    {
-        public int id { get; set; }
-        public string titulo { get; set; }       
+    public class Evento : BaseModel
+    {     
         public string descripcion { get; set; }
         public DateTime diaHoraRealizacion { get; set; }
         public double precio { get; set; }
         public string[] enlaces { get; set; }
-        public string foto { get; set; }
         public int id_establecimiento { get; set; }
         public string nombre_establecimiento { get; set; }
 
