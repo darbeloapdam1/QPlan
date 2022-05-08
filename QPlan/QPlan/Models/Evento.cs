e6686f82419a06b5f8db9ca9abe2af415580345a
darbeloapdam1 <darbeloapdam1@gmail.com>
2022-05-09 00:20:17 +0200

    modificar model evento

--- QPlan/QPlan/Models/Evento.cs
+++ QPlan/QPlan/Models/Evento.cs
@@ -7,22 +7,14 @@ namespace QPlan.Models
     public class Evento : BaseModel
     {     
         public string descripcion { get; set; }
-        public DateTime diaHoraRealizacion { get; set; }
+        public DateTime dia { get; set; }
+        public TimeSpan hora { get; set; }
         public double precio { get; set; }
         public string[] enlaces { get; set; }
         public int id_establecimiento { get; set; }
         public string nombre_establecimiento { get; set; }
-
-        public string Dia
-        {
-            get { return diaHoraRealizacion.Date.ToString(); }
-        }
-
-        public string Hora
-        {
-            get { return diaHoraRealizacion.Hour.ToString(); }
-        }
-
+        public int edad_minima { get; set; }
+        public string observaciones { get; set; }
         public Evento() { }
 
         public Evento(EventoJs ev)
