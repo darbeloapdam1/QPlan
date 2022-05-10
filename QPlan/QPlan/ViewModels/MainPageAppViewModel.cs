1763b2b3f4585e87ab00e9fb3485bbd11a543496
darbeloapdam1 <darbeloapdam1@gmail.com>
2022-05-10 20:44:32 +0200

    añadir navegacion entre paginas

--- QPlan/QPlan/ViewModels/MainPageAppViewModel.cs
+++ QPlan/QPlan/ViewModels/MainPageAppViewModel.cs
@@ -1,15 +1,31 @@
-﻿using System;
+﻿using QPlan.Views.PaginasCuentaPublicar;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace QPlan.ViewModels
 {
     public class MainPageAppViewModel : BaseViewModel
     {
+        private bool cuentaPublicar = true;
         public MainPageAppViewModel(INavigation navigation) : base(navigation)
         {
             this.Navigation = navigation;
+            LaunchApp();
+        }
+
+        private async Task LaunchApp()
+        {
+            if (cuentaPublicar)
+            {
+                await Navigation.PushModalAsync(new MainPageCuentaPublicar());
+            }
+            else
+            {
+                await Navigation.PushModalAsync(new MainPage());
+            }
         }
 
     }
